88570459b3ccbbd5e23bfa17713dbca91916443d
Sandy <sqp@andrew.cmu.edu>
2018-12-04 21:55:33 +0000

    changed alot of design stuff

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get 'home/about', to: 'home#about', as: :about
   get 'home/contact', to: 'home#contact', as: :contact
   get 'home/privacy', to: 'home#privacy', as: :privacy
+  get 'home/search', to: 'home#search', as: :search
 
   # Authentication routes
   resources :sessions
