e1f4fcf7fee021b6bc8ab3e00ee48854a510918c
Sandy <sqp@andrew.cmu.edu>
2018-12-10 19:11:46 +0000

    finishing up dashboard for commish and chief

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,8 @@ class HomeController < ApplicationController
 
   def index
     @my_notes = @current_user.officer.investigation_notes.paginate(page: params[:my_notes_page]).per_page(10)
+    @units = Unit.alphabetical.active.paginate(page: params[:page]).per_page(5)
+    @officers = Officer.alphabetical.active.paginate(page: params[:page]).per_page(5)
   end
 
   def about
